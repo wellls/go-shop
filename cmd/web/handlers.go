dd5143547e1fc65efd8219229778853893eadb0b
Wellington Lima <wellington.map@hotmail.com>
2024-07-22 15:08:15 -0300

    chore: render virtual terminal template with base layout data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,5 +3,7 @@ package main
 import "net/http"
 
 func (app *application) VirtualTerminal(rw http.ResponseWriter, r *http.Request) {
-	app.infoLog.Println("Hit the handler")
+	if err := app.renderTemplate(rw, r, "terminal", nil); err != nil {
+		app.errorLog.Println(err)
+	}
 }
